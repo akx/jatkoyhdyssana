e85cea6c5d94a328f8aee333a3fa48095183d903
Aarni Koskela <akx@iki.fi>
2020-09-22 20:21:41 +0300

    Optimize with candidate word set

--- jatkoyhdys.py
+++ jatkoyhdys.py
@@ -1,11 +1,23 @@
-words = open("kotus_sanat.txt", "r", encoding="utf-8").read().splitlines()
-words = [w for w in words if w[0].isalpha()]
+import tqdm
+from collections import defaultdict
 
-for w1 in words:
+with open("kotus_sanat.txt", "r", encoding="utf-8") as fp:
+    words = [w.strip() for w in fp if w[0].isalpha()]
+
+words_by_letters = defaultdict(set)
+for word in words:
+    for letter in word:
+        words_by_letters[letter].add(word)
+
+for w1 in tqdm.tqdm(words):
     ends = [w1[-n:] for n in range(3, min(len(w1), 15))]
-    for w2 in words:
-        if w1 == w2:
-            continue
+
+    cand_words = set()
+    for letter in w1:
+        cand_words.update(words_by_letters[letter])
+    cand_words.discard(w1)
+
+    for w2 in cand_words:
         for end in ends:
             # if end not in words:
             # 	continue
