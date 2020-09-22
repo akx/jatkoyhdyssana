9245aafed48a98668728d3c81116b934a08f8cc9
Aarni Koskela <akx@iki.fi>
2020-09-22 20:23:18 +0300

    Wrap in main dance

--- jatkoyhdys.py
+++ jatkoyhdys.py
@@ -1,4 +1,5 @@
 import tqdm
+
 from collections import defaultdict
 
 with open("kotus_sanat.txt", "r", encoding="utf-8") as fp:
@@ -9,14 +10,14 @@ for word in words:
     for letter in word:
         words_by_letters[letter].add(word)
 
-for w1 in tqdm.tqdm(words):
-    ends = [w1[-n:] for n in range(3, min(len(w1), 15))]
 
+def process_word(w1: str):
+    ends = [w1[-n:] for n in range(3, min(len(w1), 15))]
     cand_words = set()
     for letter in w1:
         cand_words.update(words_by_letters[letter])
     cand_words.discard(w1)
-
+    results = set()
     for w2 in cand_words:
         for end in ends:
             # if end not in words:
@@ -24,4 +25,15 @@ for w1 in tqdm.tqdm(words):
             if w2.startswith(end):
                 nw = w1[: -len(end)] + w2
                 if nw not in (w1, w2):
-                    print(nw)
+                    results.add(nw)
+    return results
+
+
+def main():
+    for w1 in tqdm.tqdm(words):
+        for result in process_word(w1):
+            print(result)
+
+
+if __name__ == "__main__":
+    main()
